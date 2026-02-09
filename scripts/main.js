166ad2ac7a0b0e5570d11268188cd716e1371b99
FellipeAlvesBritoOficial <fellipealvesbrito09@gmail.com>
2026-02-09 14:14:05 -0300

    feito o gráfico das skills

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,25 @@
 // grafico
-let pct = [70, 90, 100, 80]
-let colors = ['--color_blue', '--color_maya-blue', '--color_prussian-blue', '--color_blue']
+let pct = [90, 100, 80, 100]
+let colors = ['--color_blue', '--color_maya-blue', '--color_blue', '--color_maya-blue', '--color_blue']
 let bars = document.getElementsByClassName('bar')
 let c = 0
 const root = document.documentElement
 const estiloComputado = getComputedStyle(root)
-const corAtual = estiloComputado.getPropertyValue(colors[c])
+//const corAtual = estiloComputado.getPropertyValue(colors[c])
 //root.style.setProperty('--cor-principal', 'blue');
 for (bar of bars) {
     bar.style.gridRow = `${11 - (pct[c] / 10)} / -2`
     //bar.style.gridColumn = `${c} / span 1`
-    bar.style.backgroundColor = estiloComputado.getPropertyValue(colors[c])
+    bar.style.backgroundImage = `linear-gradient(to bottom, ${estiloComputado.getPropertyValue(colors[c+1])}, ${estiloComputado.getPropertyValue(colors[c])})`
     c++
+
+    //bar.addEventListener('mouseover', hover_pct)
 }
+
+// onmouse grafico
+
+/*function hover_pct() {
+    let pct = document.querySelectorAll('#skills ul#pct > li')
+    if ()
+    
+}*/
